ee72eefdaf0568956460bb54c6fd51fbd860ddcd
sumitchowdhury512 <sumitchowdhury9971@gmail.com>
2023-05-31 15:09:00 +0530

    Completed

--- lib/screens/note_editor.dart
+++ lib/screens/note_editor.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:Note/style/app_style.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class NoteEditorScreen extends StatefulWidget {
   const NoteEditorScreen({Key? key}) : super(key: key);
@@ -9,11 +13,52 @@ class NoteEditorScreen extends StatefulWidget {
 }
 
 class _NoteEditorScreenState extends State<NoteEditorScreen> {
-
+  int color_id = Random().nextInt(AppStyle.cardsColor.length);
+  String date = DateTime.now().toString();
+  TextEditingController _title = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppStyle.cardsColor[color_id],
+      appBar: AppBar(
+        backgroundColor: AppStyle.cardsColor[color_id],
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Add a new Note",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: _title,
+              decoration: InputDecoration(
+                  border: InputBorder.none, helperText: "Note Title"),
+              style: AppStyle.mainTitle,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppStyle.accentColor,
+        child: Icon(Icons.save),
+        onPressed: () {
 
+          var uuid = Uuid();
+          FirebaseFirestore.instance.collection("Notes").add({
+            "uniq_id": uuid.v1(),
+            "check_box": false,
+            "note_title": _title.text,
+            "creation_date": date,
+            "color_id": color_id
+          }).then((value) => Navigator.pop(context));
+
+              // .catchError((e) => print("fails" + e.toString()));
+        },
+      ),
     );
   }
 }
